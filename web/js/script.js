4969acd20f7b1fcc80cbe6e1fd33e073ef3ea28d
iviakc <admisas@mail.ru>
2016-01-08 21:06:43 +0300

    lesson 4 net paginacii

--- web/js/script.js
+++ web/js/script.js
@@ -1,14 +1,9 @@
 /**
  * Created by IVIAKC on 06.11.2015.
  */
-function addLi(text){
-    var str_creat = document.createElement('li');
-    var sec_in;
-    sec_in = document.getElementById('ol_id');
-    sec_in.appendChild(str_creat);
-    str_creat.innerHTML = text;
-}
-
+var text_doc;
+var j=1;
+var writ;
 function tab_load(){
     var ajax = new XMLHttpRequest();
     ajax.open('GET','http://smart-route.ru:8100/adapter-web/rest/dictionary/c580d006-f86f-4bdd-84be-b51de6f626c6',false);
@@ -16,42 +11,67 @@ function tab_load(){
 
     var response = ajax.responseText;
     text = JSON.parse(response);
-    text_doc = text.documents;
+    writ = text.documents;
+    text_doc=writ;
+    add();
+}
+
+function compareAge(personA, personB) {
+    return personA.fio > personB.fio;
+}
+
+function Search(){
+
+    var v = document.getElementById('search').value;
+    console.log("Index: " + v);
+    writ = text_doc.filter(function(number){
+        console.log("Index: " + number.fio);
+        console.log("Index: " + number.fio.indexOf(v));
+        if(number.fio.indexOf(v)>=0) return true;
+        else return false;});
+    writ.sort(compareAge);
+    del();
+    add();
+
+}
+
+function add(){
+    console.log("Add work");
     var tab = document.getElementById('table_in');
-    var j=1;
-    text_doc.forEach(function(item){
+    j=1;
+    writ.forEach(function(item){
         var tr = document.createElement('tr');
+        tr.id='bod';
         tab.appendChild(tr);
         for(var i=0;i<5;i++){
             var td = document.createElement('td');
-            td.id='td_'+i;
-            if(i==0)
-                td.innerHTML = j;
-            if(i==1)
-                td.innerHTML = item.fio;
-            if(i==2)
-                td.innerHTML = item.gender;
-            if(i==3)
-                td.innerHTML = item.birthDate;
-            if(i==4)
-                td.innerHTML = item.phone;
+            switch (i) {
+                case 0:
+                    td.innerHTML = j;
+                    break;
+                case 1:
+                    td.innerHTML = item.fio;
+                    break;
+                case 2:
+                    td.innerHTML = item.gender;
+                    break;
+                case 3:
+                    td.innerHTML = item.birthDate;
+                    break;
+                case 4:
+                    td.innerHTML = item.phone;
+                    break;
+            }
             tr.appendChild(td);
         }
         j++;
     });
-}
-function test(){
-    alert('asd');
-}
-function send_sub(){
 
-    var ajax = new XMLHttpRequest();
-    var x = document.getElementById('inp_log').value;
-    var z = document.getElementById('inp_pass').value;
-    var url = 'http://localhost:8080/webservis/login?username=' + x + '&password=' + z;
-    ajax.open('GET',url,false);
-    ajax.send();
+}
+function del() {
+    for (var i = 1; i < j; i++) {
+        var el = document.getElementById('bod');
+        el.parentNode.removeChild(el);
+    }
 
-    var response = ajax.responseText;
-    alert(response);
 }
