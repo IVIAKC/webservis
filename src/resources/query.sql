760e2f92438c8d73412da6d78a78c1c5f415bd10
iviakc <admisas@mail.ru>
2016-01-10 18:41:08 +0300

    Результат(Поправлена структура проекта)

---
+++ src/resources/query.sql
@@ -0,0 +1,24 @@
+CREATE TABLE mytable (
+    id  	    SERIAL PRIMARY KEY,
+    login           varchar(20),
+    pas	    varchar(20),
+    first_name       varchar(20),
+    last_name        varchar(20)
+);
+
+INSERT INTO mytable(login,password,first_name,last_name)
+VALUES ('IVIAKC','qwe','Max','Bytorin');
+
+INSERT INTO mytable(login,password,first_name,last_name)
+VALUES ('eXerious','asd','Dmitr','Bytorin');
+
+INSERT INTO mytable(login,password,first_name,last_name)
+VALUES ('Devostator','zxc','Artem','Kicel');
+
+INSERT INTO mytable(login,password,first_name,last_name)
+VALUES ('D','zxc','Artem','Kicel');
+
+INSERT INTO mytable(login,password,first_name,last_name)
+VALUES ('Kir','wer','Василий','Бабаевич');
+
+DROP table mytable;
